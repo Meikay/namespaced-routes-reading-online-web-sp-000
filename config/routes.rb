e75455accbee3032d9fd351823886e6514efa5cb
Meisuleen Kased <meisuleen@gmail.com>
2019-10-22 14:05:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :authors, only: %i[show index] do
     resources :posts, only: %i[show index new edit]
   end
-  
+
   resources :posts, only: %i[index show new create edit update]
 
   scope '/admin' do
